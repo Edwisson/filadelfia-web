4d9f71da6cd8c61c02046621af7c8ce56c84dedb
Remanuel <edwisson.m.r.l@gmail.com>
2024-06-11 19:07:33 -0400

    Barra de navegacion

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>Filadelfia</title>
+</head>
+<body>
+
+    <header>
+        <div class="contenedor">
+            <img src="{{ asset('images/logo.png') }}" alt="Filadelfia" class="logo">
+            <nav class="navegacion_principal">
+                <a href="#">Inicio</a>
+                <a href="#">Alas de Miranda</a>
+                <a href="#">Contacto</a>
+                <a href="#">Donar</a>
+                <a href="#">Recursos</a>
+            </nav>
+        </div>
+    </header>
+
+    <section class="banner">
+        <div class="cont_banner">
+            <h1>
+                Fieles de Filadelfia
+            </h1>
+            <h2>
+
+            </h2>
+        </div>
+    </section>
+    <div>
+       <h1>hola</h1>
+    </div>
+</body>
+</html>
