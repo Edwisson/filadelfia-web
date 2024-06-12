7f892e03336ba4115f7f439780b9720d9852ec99
Ender <endermunoz18@gmail.com>
2024-06-12 16:28:38 -0400

    rollback de la tabla contacto, cree Modelos Migraciones, Ejecute el migrate. se creo la tabla contactos con s.

--- database/migrations/2024_06_12_150022_create_contactos_table.php
+++ database/migrations/2024_06_12_150022_create_contactos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contacto', function (Blueprint $table) {
+        Schema::create('contactos', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
             $table->string('apellido');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contacto');
+        Schema::dropIfExists('contactos');
     }
 };
