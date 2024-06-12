f6fae059aa6e3159250bd4e18660f20d4c3c6c2f
Remanuel <edwisson.m.r.l@gmail.com>
2024-06-12 13:58:34 -0400

    Banner principal 80%

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    
     <title>Filadelfia</title>
 </head>
 <body>
 
     <header>
-        <div class="contenedor">
+        <div class="contenedor margin_container">
             <img src="{{ asset('images/logo.png') }}" alt="Filadelfia" class="logo">
             <nav class="navegacion_principal">
                 <a href="#">Inicio</a>
@@ -25,14 +29,17 @@
         <div class="font_banner">
             <div class="grid_banner">
                 <div class="titulo_banner">
-                    <h1>Fieles de <br> filadelfia</h1>
+                    <h1>Fieles de <br> FILADELFIA</h1>
                     <div> 
 
                     </div>
                 </div>
                 <div class="content_banner">
-                    <h2>Donde el amor a Dios y a la familia es lo mas importante</h2>
+                    <h2>Donde el amor a Dios y a la familia <br> es lo mas importante</h2>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum dicta labore illum itaque expedita! At ad quibusdam voluptas, illum dolores repellat iste voluptatibus, officia dolorum dolor, inventore saepe numquam a.</p>
+                    <div class="boton_unir">
+                        <a href="#">Unetenos!</a>
+                    </div>
                 </div>
             </div>
         </div>
